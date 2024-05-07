31174eceeeea04223a184e38633a7d5d35551636
Devmanwwani <devrmanwani@gmail.com>
2024-05-07 06:21:10 +0530

    added api

---
+++ src/api.ts
@@ -0,0 +1,42 @@
+export interface Job{
+    jdUid: string;
+    jdLink: string;
+    jobDetailsFromCompany: string;
+    maxJdSalary: number;
+    minJdSalary: number;
+    SalaryCurrencyCode:string;
+    location: string;
+    minExp:string;
+    maxExp:string;
+    jobRole: string;
+    companyName: string;
+    logoUrl:string;
+}
+
+export const fetchJobsAPI = async (offset:number): Promise<Job[]> => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const body = JSON.stringify({
+        limit: 10, 
+        offset
+    });
+
+    try{
+        const response = await fetch("https://api.weekday.technology/adhoc/getSampleJdJSON", {
+            method: 'POST',
+            headers: myHeaders,
+            body
+        });
+
+        if(!response.ok){
+            throw new Error('Failed to fetch jobs');
+        }
+
+        const data = await response.json();
+        return data.jdList;
+
+    }catch(err:any){
+        throw new Error(err.message);
+    }
+};
